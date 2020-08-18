d31d7a6ffbdaab45ab9a2ed7cb747adc700e9df9
PamelaValenzuela <pamela.valenzuela@usach.cl>
2020-08-18 15:21:06 -0400

    second commit

---
+++ app/src/main/java/com/example/martes18/TaskDataBase.kt
@@ -0,0 +1,35 @@
+package com.example.martes18
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+//añadir nombre y entities, y las TaskDao(une los Daos y las entidades)
+
+@Database(entities = arrayOf(Task::class), version = 1, exportSchema = false )
+abstract class TaskDataBase: RoomDatabase(){
+
+    abstract fun getTaskDao(): TaskDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: TaskDataBase?= null
+
+        fun getDataBase(context : Context): TaskDataBase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance= Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDataBase::class.java,
+                    "task_database").build()
+            INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
